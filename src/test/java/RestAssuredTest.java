9c9424356887a3345e93e798b1e34ccdd60d2075
d.muminov <dostonbek.andijonlik@gmail.com>
2025-01-22 19:53:38 +0500

    RestAssured project

--- src/test/java/RestAssuredTest.java
+++ src/test/java/RestAssuredTest.java
@@ -1,5 +1,7 @@
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RestAssuredTest {
@@ -7,12 +9,22 @@ public class RestAssuredTest {
     @Test
     void test1(){
 
-        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        Response response = get("https://reqres.in/api/users?page=2");
         System.out.println("Response: " + response.asString());
         System.out.println("STATUS CODE: " + response.getStatusCode());
         System.out.println("Body: " + response.getBody().asString());
         System.out.println("Get Time: " + response.getTime());
         System.out.println("Get Header: " + response.getHeader("content-type"));
 
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+
+    @Test
+    void test2(){
+        given().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                statusCode(200);
     }
 }
